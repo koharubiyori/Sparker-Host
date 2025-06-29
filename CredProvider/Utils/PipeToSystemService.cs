a1eba8cac7adb225e4540615cccaf3338e39620a
koharubiyori <a240498075@gmail.com>
2025-06-29 14:18:39 +0800

    feat(credProvider): enable the named pipe to accept multiple clients

--- CredProvider/Utils/PipeToSystemService.cs
+++ CredProvider/Utils/PipeToSystemService.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Pipes;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using SparkerCommons;
 using SparkerCommons.Utils;
@@ -11,130 +13,221 @@ namespace SparkerCredProvider.Utils
   public static class PipeToSystemService
   {
     private const string PipeLabel = "PipeToSystemService";
-    public static Action<string, string> OnUserInfoReceived = null;
+    private static readonly List<PipeClientHandler> _clients = new List<PipeClientHandler>();
+    private static readonly object _clientLock = new object();
+    private static CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+    public static Action<string, string> OnUserInfoReceived { get; set; }
 
-    private static NamedPipeServerStream _pipeServer;
-    private static bool _stopFlag;
-
-    public static async Task Run()
+    public static async Task RunAsync()
     {
-      _stopFlag = false;
-      while (!_stopFlag)
+      Logger.Write($"{PipeLabel}: Starting to listen for client connections...");
+
+      while (!_cancellationTokenSource.IsCancellationRequested)
       {
-        _pipeServer = new NamedPipeServerStream(Constants.CredPipeName, PipeDirection.InOut, 1,
-          PipeTransmissionMode.Message, PipeOptions.Asynchronous);
-        
+        Logger.Write("test");
+        NamedPipeServerStream pipeServer = null;
         try
         {
-          Logger.Write($"{PipeLabel}: Waiting for the connection...");
-          await _pipeServer.WaitForConnectionAsync();
-          Logger.Write($"{PipeLabel}: Connected!");
+          pipeServer = new NamedPipeServerStream(
+            Constants.CredPipeName,
+            PipeDirection.InOut,
+            NamedPipeServerStream.MaxAllowedServerInstances,
+            PipeTransmissionMode.Message,
+            PipeOptions.Asynchronous);
+
+          Logger.Write($"{PipeLabel}: Waiting for a new connection...");
+
+          using (_cancellationTokenSource.Token.Register(() => pipeServer.Dispose()))
+          {
+            await pipeServer.WaitForConnectionAsync(_cancellationTokenSource.Token);
+          }
+
+          var handler = new PipeClientHandler(pipeServer, OnUserInfoReceived);
+          lock (_clientLock)
+          {
+            _clients.Add(handler);
+          }
+
+          var unused = handler.RunAsync(() =>
+          {
+            lock (_clientLock)
+            {
+              _clients.Remove(handler);
+            }
+          });
+
+          Logger.Write($"{PipeLabel}: Client connected. Total clients: {_clients.Count}");
+        }
+        catch (OperationCanceledException)
+        {
+          Logger.Write($"{PipeLabel}: The listener loop was canceled.");
+          break;
         }
         catch (Exception ex)
         {
-          if (_stopFlag) continue;
-          Logger.Write($"{PipeLabel}: Failed to connect: {ex.Message}");
-          await Task.Delay(500);
-          continue;
+          if (_cancellationTokenSource.IsCancellationRequested)
+          {
+            Logger.Write($"{PipeLabel}: The listener loop was canceled.");
+            break;
+          }
+
+          Logger.Write($"{PipeLabel}: An error occurred in the listener loop. Retrying in 1 second.");
+          pipeServer?.Dispose();
+          await Task.Delay(1000, _cancellationTokenSource.Token);
         }
-        
-        await ReadLoop();
       }
     }
 
     public static void Stop()
     {
-      _stopFlag = true;
-      Clean();
+      _cancellationTokenSource.Cancel();
     }
 
-    private static async Task WritePong()
+    public static Task WriteUnlockRequestToAllClients()
     {
-      await Write(Constants.CredPipeMessageType.Out.Pong);
-      Logger.Write($"{PipeLabel}: Sent pong!");
-    }
-    
-    public static async Task WriteUnlockRequest()
-    {
-      await Write(Constants.CredPipeMessageType.Out.RequestToUnlock);
-      Logger.Write($"{PipeLabel}: Sent unlock request!");
-    }
-
-    private static async Task ReadLoop()
-    {
-      var buffer = new byte[1024];
-
-      try
+      lock (_clientLock)
       {
-        while (true)
+        Logger.Write($"{PipeLabel}: Broadcasting unlock request to {_clients.Count} clients.");
+        var clientCopy = _clients.ToArray();
+        var writeTasks = clientCopy.Select(client => client.WriteUnlockRequestAsync());
+        return Task.WhenAll(writeTasks);
+      }
+    }
+
+    public static Task DisposeAsync()
+    {
+      if (_cancellationTokenSource == null) return Task.FromResult(0);
+
+      Logger.Write($"{PipeLabel}: Disposing and stopping all connections...");
+      _cancellationTokenSource.Cancel();
+
+      PipeClientHandler[] clientCopy;
+      lock (_clientLock)
+      {
+        clientCopy = _clients.ToArray();
+      }
+
+      var shutdownTasks = clientCopy.Select(client => client.DisposeAsync());
+      var whenAllTask = Task.WhenAll(shutdownTasks);
+
+      whenAllTask.ContinueWith(t =>
+      {
+        _cancellationTokenSource.Dispose();
+        _cancellationTokenSource = null;
+      });
+
+      return whenAllTask;
+    }
+
+    private class PipeClientHandler
+    {
+      private readonly NamedPipeServerStream _pipeStream;
+      private readonly Action<string, string> _onUserInfoReceived;
+
+      public PipeClientHandler(NamedPipeServerStream pipeStream, Action<string, string> onUserInfoReceived)
+      {
+        _pipeStream = pipeStream;
+        _onUserInfoReceived = onUserInfoReceived;
+      }
+
+      public async Task RunAsync(Action onDisconnected)
+      {
+        try
         {
-          var bytesRead = await _pipeServer.ReadAsync(buffer, 0, buffer.Length);
-          if (bytesRead == 0)
-          {
-            Logger.Write($"{PipeLabel}: The connection is closed!");
-            return;
-          }
-          
-          var unpackedMessages = StringArrayPacker.Unpack(buffer.Take(bytesRead).ToArray());
+          await ReadLoopAsync();
+        }
+        catch (Exception ex)
+        {
+          Logger.Write($"{PipeLabel}: An unhandled exception occurred in the read loop.\n{ex}");
+        }
+        finally
+        {
+          onDisconnected?.Invoke();
+          await DisposeAsync();
+        }
+      }
+
+      private async Task ReadLoopAsync()
+      {
+        var buffer = new byte[1024];
+        while (_pipeStream.IsConnected)
+        {
+          var bytesRead = await _pipeStream.ReadAsync(buffer, 0, buffer.Length);
+          if (bytesRead == 0) break;
+
+          var receivedData = new byte[bytesRead];
+          Array.Copy(buffer, 0, receivedData, 0, bytesRead);
+          var unpackedMessages = StringArrayPacker.Unpack(receivedData);
+
           foreach (var message in unpackedMessages)
           {
-            if (await HandleMessage(message)) return;
-            await _pipeServer.FlushAsync();
+            await HandleMessageAsync(message);
           }
         }
       }
-      catch (Exception e)
-      {
-        if (!(e is IOException) && !(e is OperationCanceledException) && !(e is ObjectDisposedException)) throw;
-        Logger.Write($"{PipeLabel}: Pipe closed or broken!");
-      }
-    }
 
-    private static async Task<bool> HandleMessage(string[] message)
-    {
-      var messageType = message[0];
-      Logger.Write($"{PipeLabel}: Received Message: {messageType}");
-          
-      switch (messageType)
+      private Task HandleMessageAsync(string[] message)
       {
-        case Constants.CredPipeMessageType.In.Logon:
+        var messageType = message[0];
+        Logger.Write($"{PipeLabel}: Received Message: {messageType}");
+
+        switch (messageType)
         {
-          var username = message[1];
-          var password = message[2];
-          OnUserInfoReceived?.Invoke(username, password);
-          break;
+          case Constants.CredPipeMessageType.In.Logon:
+            var username = message[1];
+            var password = message[2];
+            _onUserInfoReceived?.Invoke(string.Format("{0}\\{1}", Environment.MachineName, username), password);
+            break;
+          case Constants.CredPipeMessageType.In.Ping:
+            return WritePongAsync();
         }
-        case Constants.CredPipeMessageType.In.Ping:
-          await WritePong();
-          break;
+
+        return Task.FromResult(0);
       }
 
-      return false;
-    }
-
-    private static void Clean()
-    {
-      if (_pipeServer != null)
+      public Task WriteUnlockRequestAsync()
       {
-        if (_pipeServer.IsConnected) _pipeServer.Disconnect();
-        _pipeServer.Dispose();
+        return WriteAsync(Constants.CredPipeMessageType.Out.RequestToUnlock)
+          .ContinueWith(t => Logger.Write($"{PipeLabel}: Sent unlock request!"));
       }
-      
-      _pipeServer = null;
-    }
 
-    private static async Task Write(params string[] items)
-    {
-      var packagedData = StringArrayPacker.Pack(items);
-      try
+      private Task WritePongAsync()
       {
-        await _pipeServer.WriteAsync(packagedData, 0, packagedData.Length);
-        await _pipeServer.FlushAsync();
+        return WriteAsync(Constants.CredPipeMessageType.Out.Pong)
+          .ContinueWith(t => Logger.Write($"{PipeLabel}: Sent pong!"));
       }
-      catch (IOException e)
+
+      private async Task WriteAsync(params string[] items)
       {
-        Logger.Write($"{PipeLabel}: The pipe has been closed or broken!");
-        Clean();
+        if (!_pipeStream.IsConnected) return;
+        var packagedData = StringArrayPacker.Pack(items);
+        try
+        {
+          await _pipeStream.WriteAsync(packagedData, 0, packagedData.Length);
+          await _pipeStream.FlushAsync();
+        }
+        catch (IOException ex)
+        {
+          Logger.Write($"{PipeLabel}: Failed to write. The pipe has been closed or broken.\n{ex}");
+        }
+      }
+
+      public Task DisposeAsync()
+      {
+        if (_pipeStream.IsConnected)
+        {
+          try
+          {
+            _pipeStream.Disconnect();
+          }
+          catch (Exception ex)
+          {
+            Logger.Write($"{PipeLabel}: Error during disconnect.");
+          }
+        }
+
+        _pipeStream.Dispose();
+        return Task.FromResult(0);
       }
     }
   }
